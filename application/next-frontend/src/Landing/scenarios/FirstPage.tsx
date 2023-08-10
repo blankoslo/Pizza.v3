7302781d8ec485dcc343a3f2e51865de443bb589
Sergey <thefanthomasfan@gmail.com>
2023-08-10 16:08:04 +0200

    cleaned up after code review, added _document.tsx in pages-folder

--- application/next-frontend/src/Landing/scenarios/FirstPage.tsx
+++ application/next-frontend/src/Landing/scenarios/FirstPage.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
 import Hello from '@/Landing/assets/illustrations/hello.svg'
+// import { inter, spaceGoretsk } from "@/_app"
 
 const FirstPage = () => {
     return (
         <div className="flex h-fit justify-center bg-teal-400 pb-12">
             <div className="flex w-4/5">
                 <div className="flex w-1/2 flex-col">
-                    <div className="main-text mb-5 text-6xl text-gray-900">
+                    <div className="font-spaceGrotesk mb-5 text-6xl text-gray-900 leading-[4rem]">
                         The sexiest bot for socialising teams on slack through Pizza..
                     </div>
 
-                    <div className="sub-text text-xl leading-7 text-black">
+                    <div className="font-inter text-xl leading-7 text-black">
                         Once upon a time, in a land where disco balls still reigned supreme, there strutted a human
                         tornado of coolness named Max Chillington. Legend has it that Max could make ice cubes shiver in
                         envy just by winking. <br /> <br />
--- application/next-frontend/tailwind.config.ts
+++ application/next-frontend/tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
             },
             fontFamily: {
                 spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
             },
         },
         plugins: [],
