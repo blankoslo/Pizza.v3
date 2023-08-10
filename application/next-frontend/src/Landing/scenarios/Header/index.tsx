58ee22bed1a43f1926fdaf9f53a7d091600d927b
stianjsu <stianjsu@gmail.com>
2023-08-10 14:38:33 +0200

    PB-14m: adds empty div behind nav

--- application/next-frontend/src/Landing/scenarios/Header/index.tsx
+++ application/next-frontend/src/Landing/scenarios/Header/index.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
-import PizzaBotTitle from '@/Landing/assets/illustrations/PizzaBotTitle.svg'
-import { AddToSlackButton } from './components/AddToSlackButton'
+import Image from 'next/image';
+import PizzaBotTitle from '@/Landing/assets/illustrations/PizzaBotTitle.svg';
+import { AddToSlackButton } from './components/AddToSlackButton';
 
 const Header = () => {
     return (
-        <nav className="header flex flex-row items-center gap-8">
-            <div className="flex-1">
-                <Image src={PizzaBotTitle} width={300} alt="Pizza Bot" />
-            </div>
-            <div>
-                <button className="p-4 font-bold hover:bg-gray-300">About us</button>
-            </div>
-            <AddToSlackButton />
-        </nav>
-    )
-}
+        <>
+            <div className="header !static"></div>
+            <nav className="header flex flex-row items-center gap-8">
+                <div className="flex-1">
+                    <Image src={PizzaBotTitle} width={300} alt="Pizza Bot" />
+                </div>
+                <div>
+                    <button className="p-4 font-bold hover:bg-gray-300">About us</button>
+                </div>
+                <AddToSlackButton />
+            </nav>
+        </>
+    );
+};
 
-export { Header }
+export { Header };
