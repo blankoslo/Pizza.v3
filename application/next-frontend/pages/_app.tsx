5f524ae89f3a35c3a2df71364de8889aa07a04a8
stianjsu <stianjsu@gmail.com>
2023-08-10 13:01:00 +0200

    refactor(styles): fixes styles config for app

--- application/next-frontend/pages/_app.tsx
+++ application/next-frontend/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@/styles/globals.css';
+import '@/styles/reset.css';
+import '@/styles/tailwindComposables.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
