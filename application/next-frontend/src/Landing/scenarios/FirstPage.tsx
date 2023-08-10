1de2413eb1ff5a39b0daad6e9cdbda4e0c8dd0a7
Sergey <thefanthomasfan@gmail.com>
2023-08-10 14:32:15 +0200

    changed setup for styling page

--- application/next-frontend/src/Landing/scenarios/FirstPage.tsx
+++ application/next-frontend/src/Landing/scenarios/FirstPage.tsx
@@ -4,20 +4,24 @@ import Hello from '@/Landing/assets/illustrations/hello.svg'
 const FirstPage = () => {
     return (
         <div className="root">
-            <div className="text-container">
-                <div className="main-text">The sexiest bot for socialising teams on slack through Pizza..</div>
+            <div className="wrapper">
+                <div className="text-container">
+                    <div className="main-text">
+                        The sexiest bot for socialising teams on slack through Pizza..
+                    </div>
 
-                <div className="sub-text">
-                    Once upon a time, in a land where disco balls still reigned supreme, there strutted a human tornado
-                    of coolness named Max Chillington. Legend has it that Max could make ice cubes shiver in envy just
-                    by winking. <br /> <br />
-                    His hair, a perfectly coiffed monument to follicular fortitude, once caused a gust of wind to bow in
-                    admiration.
+                    <div className="sub-text">
+                        Once upon a time, in a land where disco balls still reigned supreme, there strutted a human tornado
+                        of coolness named Max Chillington. Legend has it that Max could make ice cubes shiver in envy just
+                        by winking. <br /> <br />
+                        His hair, a perfectly coiffed monument to follicular fortitude, once caused a gust of wind to bow in
+                        admiration.
+                    </div>
                 </div>
-            </div>
 
-            <div className="firstpage-img">
-                <Image priority src={Hello} height={435} width={566} alt="bot saying hello" />
+                <div className="firstpage-img">
+                    <Image priority src={Hello} width={350} alt="bot saying hello" />
+                </div>
             </div>
         </div>
     )
