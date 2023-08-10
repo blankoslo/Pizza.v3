05a84db4bb5dfbb43d62afe90f5a7f13baf52165
Sergey <thefanthomasfan@gmail.com>
2023-08-10 15:30:31 +0200

    done with first iteration of landing page

--- application/next-frontend/src/Landing/scenarios/FirstPage.tsx
+++ application/next-frontend/src/Landing/scenarios/FirstPage.tsx
@@ -3,24 +3,25 @@ import Hello from '@/Landing/assets/illustrations/hello.svg'
 
 const FirstPage = () => {
     return (
-        <div className="root">
-            <div className="wrapper">
-                <div className="text-container">
-                    <div className="main-text">
+        <div className="flex h-fit justify-center bg-teal-400 pb-12">
+            <div className="flex w-4/5">
+                <div className="flex w-1/2 flex-col">
+                    <div className="main-text mb-5 text-6xl text-gray-900">
                         The sexiest bot for socialising teams on slack through Pizza..
                     </div>
 
-                    <div className="sub-text">
-                        Once upon a time, in a land where disco balls still reigned supreme, there strutted a human tornado
-                        of coolness named Max Chillington. Legend has it that Max could make ice cubes shiver in envy just
-                        by winking. <br /> <br />
-                        His hair, a perfectly coiffed monument to follicular fortitude, once caused a gust of wind to bow in
-                        admiration.
+                    <div className="sub-text text-xl leading-7 text-black">
+                        Once upon a time, in a land where disco balls still reigned supreme, there strutted a human
+                        tornado of coolness named Max Chillington. Legend has it that Max could make ice cubes shiver in
+                        envy just by winking. <br /> <br />
+                        His hair, a perfectly coiffed monument to follicular fortitude, once caused a gust of wind to
+                        bow in admiration.
                     </div>
                 </div>
 
-                <div className="firstpage-img">
-                    <Image priority src={Hello} width={350} alt="bot saying hello" />
+                {/* problem: my-auto doesnt care about padding on bottom */}
+                <div className="m-auto">
+                    <Image priority src={Hello} width={450} alt="bot saying hello" />
                 </div>
             </div>
         </div>
