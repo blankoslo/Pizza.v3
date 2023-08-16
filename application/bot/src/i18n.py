8b54b56b03fe35a8e99ecb294baca56df1b3db2c
Jacob Theisen <jacobth@stud.ntnu.no>
2023-08-16 16:12:20 +0200

    PB-29b & PB-29d: functionality for multiple lang + default english

---
+++ application/bot/src/i18n.py
@@ -0,0 +1,40 @@
+import json
+import os
+import glob
+from src.injector import injector
+import logging
+from string import Template
+
+
+
+supported_format = ["json"]
+
+class Translator:
+    def __init__(self, language_folder="./lang", default_locale="en") -> None:
+        self.data= {}
+        self.locale = default_locale
+        self.logger = injector.get(logging.Logger)
+
+
+        for filename in glob.glob(os.path.join(language_folder, '*.json')):
+            loc = os.path.splitext(os.path.basename(filename))[0]
+            with open(filename, encoding="utf-8", mode="r") as f:
+                self.data[loc] = json.load(f)
+
+    def set_locale(self, locale):
+        if locale in self.data:
+            self.locale = locale
+        else:
+            self.logger.warn(f"Unvalid locale: {locale}, fallback to default locale: {self.locale}")
+
+    def translate(self, key, **kwargs): 
+        self.logger.warn(key, self.locale)
+        self.logger.warn(self.data)
+        if key in self.data[self.locale]:
+            text = self.data[self.locale][key]
+            return Template(text).safe_substitute(**kwargs)
+        else:
+            self.logger.info(f"The key '{key}' does not match any text. Defaults text to key")
+            return key
+
+
