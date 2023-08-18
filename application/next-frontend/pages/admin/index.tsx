10e8aad74e65bad3f99a94826c4de4d98675bbfb
Stian Sulebak <stianjsu@gmail.com>
2023-08-18 09:28:31 +0200

    PB-17d: Adds decoding of user when entering the admin page

--- application/next-frontend/pages/admin/index.tsx
+++ application/next-frontend/pages/admin/index.tsx
@@ -1,6 +1,38 @@
-import { Home } from 'Admin/scenarios/Home'
+import { GetServerSideProps } from 'next'
+import jwtDecode from 'jwt-decode'
+import { Home } from '@/Admin/scenarios/Home'
 
-const Adminpage = () => {
+interface Token {
+    fresh: string
+    iat: string
+    jti: string
+    type: string
+    sub: string
+    nbf: string
+    exp: string
+    user: {
+        email: string
+        picture: string
+        id: string
+        name: string
+        roles: Array<string>
+    }
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const jwt = req.cookies['access_token_cookie']
+    if (!jwt) return { redirect: { destination: '/login', permanent: false }, props: {} }
+
+    const decodedJWT = jwtDecode<Token>(jwt)
+    return {
+        props: {
+            user: decodedJWT.user,
+        },
+    }
+}
+
+const Adminpage = ({ user }: { user: Token['user'] }) => {
+    console.log(user)
     return (
         <div>
             <Home />
