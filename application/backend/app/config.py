091531819e11d89110b9a4bed03a4113ed56a9d3
Stian Sulebak <stianjsu@gmail.com>
2023-08-18 09:17:14 +0200

    PB-17h: configs for auth in new frontend

--- application/backend/app/config.py
+++ application/backend/app/config.py
@@ -26,7 +26,7 @@ class Base(object):
 
 class Test(Base):
     DEBUG = True
-    FRONTEND_URI = 'localhost:4434'
+    FRONTEND_URI = 'localhost:4000'
     SLACK_CLIENT_ID = 'dontCareSlackClientId'
     SLACK_CLIENT_SECRET = 'dontCareSlackClientSecret'
     MQ_EVENT_QUEUE = 'dontCareMqEventQueue'
