30cb90947b2efbee603a008ee86f294300088177
Stian Sulebak <stianjsu@gmail.com>
2023-08-21 13:05:57 +0200

    PB-17b: fixes bad keys in map-functions

--- application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
+++ application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
@@ -7,23 +7,26 @@ interface Restaurant {
     name: string
     address?: string
     url?: string
+    id: string
 }
 
 // Sample restaurant data
 const restaurantList: Restaurant[] = [
     {
         name: 'Example Restaurant 1',
+        id: '1',
     },
     {
         name: 'Example Restaurant 2',
+        id: '2',
     },
 ]
 
 const Restaurants = () => {
     return (
         <CardComponent title="Places" className=" w-1/4">
-            {restaurantList.map((restaurant: Restaurant, index: number) => (
-                <div key={index} className="flex items-center justify-between py-2">
+            {restaurantList.map((restaurant) => (
+                <div key={restaurant.id} className="flex items-center justify-between py-2">
                     <p>{restaurant.name}</p>
                     <button>&times;</button>
                 </div>
--- application/next-frontend/src/Admin/scenarios/Users.tsx
+++ application/next-frontend/src/Admin/scenarios/Users.tsx
@@ -3,26 +3,30 @@ import CardComponent from 'Admin/components/CardComponent'
 interface SlackUser {
     name: string
     picturePath?: string
+    id: string
 }
 
 // static data, only temp
-const slackUsersList: SlackUser[] = [
+const slackUserList: SlackUser[] = [
     {
         name: 'User 1',
+        id: '1',
     },
     {
         name: 'User 2',
+        id: '2',
     },
     {
         name: 'User 3',
+        id: '3',
     },
 ]
 
 const Users = () => {
     return (
         <CardComponent title="People" className="w-1/4">
-            {slackUsersList.map((user: SlackUser, index: number) => (
-                <div key={index} className="flex items-center justify-between py-2">
+            {slackUserList.map((user) => (
+                <div key={user.id} className="flex items-center justify-between py-2">
                     <div className="h-10 w-10 overflow-hidden rounded-full border border-black"></div>
                     <p className="ml-3">{user.name}</p>
                     <button className="ml-auto">&times;</button>
