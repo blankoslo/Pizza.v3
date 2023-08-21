25844fa769e417153255b1427a1a36e95cee706a
Stian Sulebak <stianjsu@gmail.com>
2023-08-21 12:22:44 +0200

    PB-17b: fixes undefined style set in Card-component

--- application/next-frontend/src/Adminpage/scenarios/Home/components/CardComponent.tsx
+++ application/next-frontend/src/Adminpage/scenarios/Home/components/CardComponent.tsx
@@ -8,7 +8,7 @@ const CardComponent = ({
     className?: string
 }) => {
     return (
-        <div className={`block border-2 border-black bg-white p-6 ${className}`}>
+        <div className={`block border-2 border-black bg-white p-6 ${className ?? ''}`}>
             <h5 className="mb-2 text-center font-mono text-2xl font-bold leading-tight text-neutral-800">{title}</h5>
             {children}
         </div>
