cdbe6cf07597d0902d4edb135226aeee3c86d44e
Stian Sulebak <stianjsu@gmail.com>
2023-08-21 12:19:10 +0200

    PB-17b: some cleanup of code/comments

--- application/next-frontend/src/Adminpage/scenarios/Home/components/CardComponent.tsx
+++ application/next-frontend/src/Adminpage/scenarios/Home/components/CardComponent.tsx
@@ -1,15 +1,14 @@
-interface CardComponentProps {
+const CardComponent = ({
+    title,
+    children,
+    className,
+}: {
     title: string
     children?: React.ReactNode
     className?: string
-}
-
-const CardComponent: React.FC<CardComponentProps> = ({ title, children, className }) => {
-    const defaultClassName = 'block border-2 border-black bg-white p-6'
-    const finalClassName = className ? `${defaultClassName} ${className}` : defaultClassName
-
+}) => {
     return (
-        <div className={finalClassName}>
+        <div className={`block border-2 border-black bg-white p-6 ${className}`}>
             <h5 className="mb-2 text-center font-mono text-2xl font-bold leading-tight text-neutral-800">{title}</h5>
             {children}
         </div>
--- application/next-frontend/src/Adminpage/scenarios/Navbar.tsx
+++ application/next-frontend/src/Adminpage/scenarios/Navbar.tsx
@@ -3,10 +3,6 @@ import Link from 'next/link'
 const Navbar = () => {
     const navbar = [
         { name: 'Home', link: '/admin' },
-        //{ name: 'Restaurants', link: '/admin/restaurants' }
-        // { name: 'Events', link: '/admin/events' },
-        // { name: 'People', link: '/admin/people' },
-        // { name: 'Groups', link: '/admin/groups' },
         { name: 'About', link: '/admin/about' },
         { name: 'Pictures', link: '/admin/pictures' },
     ]
@@ -14,8 +10,8 @@ const Navbar = () => {
     return (
         <div className="overflow-hidden bg-teal-400 text-gray-700">
             <ul className="flex items-center space-x-6 p-4">
-                {navbar.map((entry, index) => (
-                    <li className="m-3 cursor-pointer" key={index}>
+                {navbar.map((entry) => (
+                    <li className="m-3 cursor-pointer" key={entry.name}>
                         <Link
                             href={entry.link}
                             className="text-lg font-bold transition duration-300 hover:text-gray-500"
