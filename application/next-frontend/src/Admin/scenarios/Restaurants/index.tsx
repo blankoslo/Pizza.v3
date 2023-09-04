7cfdc19049a05dff634aec3c33b167b85b7abc3f
Sergey <thefanthomasfan@gmail.com>
2023-09-04 21:31:44 +0200

    moved every check to a big ternary

--- application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
+++ application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
@@ -7,25 +7,13 @@ import useRestaurants from '@/api/useRestaurants'
 const Restaurants = () => {
     const { data, isLoading, error } = useRestaurants()
 
-    if (isLoading) {
-        return (
-            <CardComponent title="Places" className="w-1/4">
-                Loading...
-            </CardComponent>
-        )
-    }
-
-    if (error) {
-        return (
-            <CardComponent title="Places" className="w-1/4">
-                Failed to load users due to the following error: {error?.info.msg}
-            </CardComponent>
-        )
-    }
-
     return (
         <CardComponent title="Places" className=" w-1/4">
-            {!data || data.length == 0
+            {isLoading
+                ? 'Loading...'
+                : error
+                ? `Failed to load users due to the following error: ${error?.info.msg}`
+                : !data || data.length == 0
                 ? 'No restaurants found.'
                 : data.map((restaurant) => (
                       <div key={restaurant.id} className="flex items-center justify-between py-2">
