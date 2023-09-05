91bfed9a8212892b9b8b138ffedb7b24e2f39dd1
Jacob Theisen <jacobtheisen2211@gmail.com>
2023-09-05 15:07:18 +0200

    PB-30B: Layout for mutation + toggle active status

--- application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
+++ application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
@@ -1,9 +1,8 @@
 import { CardComponent } from 'Admin/components/CardComponent'
 import useSlackUsers from '@/api/useSlackUsers'
-import { Button, Checkbox } from '@mui/material'
 
 const SlackUsers = () => {
-    const { data, isLoading, error, updateUser } = useSlackUsers()
+    const { data, isLoading, error } = useSlackUsers()
 
     if (isLoading) {
         return (
@@ -29,23 +28,15 @@ const SlackUsers = () => {
         )
     }
 
-    const updateuser = async () => {
-        const exmpUser = data[0]
-        updateUser({ ...exmpUser, active: !exmpUser.active })
-        console.log(data)
-    }
-
     return (
         <CardComponent title="People" className="w-1/4">
             {data.map((slackUser) => (
                 <div key={slackUser.slack_id} className="flex items-center justify-between py-2">
                     <div className="h-10 w-10 overflow-hidden rounded-full border border-black"></div>
                     <p className="ml-3">{slackUser.current_username}</p>
-                    <Checkbox checked={slackUser.active}></Checkbox>
                     <button className="ml-auto">&times;</button>
                 </div>
             ))}
-            <Button onClick={() => updateuser()}>Modify first user active status</Button>
         </CardComponent>
     )
 }
