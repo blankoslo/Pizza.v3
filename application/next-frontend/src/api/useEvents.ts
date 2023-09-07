b305b4d257b4e9c8f1724488ba4e934535213d80
Anna <annabjorgo0308@gmail.com>
2023-09-07 11:09:37 +0200

    wip: proposed solution to post events with helper functions

--- application/next-frontend/src/api/useEvents.ts
+++ application/next-frontend/src/api/useEvents.ts
@@ -1,5 +1,6 @@
 import { Restaurant } from './useRestaurants'
 import { useAuthedSWR } from './utils'
+// import { apiRequestHelper } from './utils' -> in another branch rn
 
 export interface Event {
     id: string
@@ -10,7 +11,25 @@ export interface Event {
 }
 
 const useEvents = () => {
-    return useAuthedSWR<Event[]>('/events')
+    const endpoint = '/events'
+    const { data, mutate } = useAuthedSWR<Event[]>(endpoint)
+    //const { post } = apiRequestHelper()
+
+    const addEvent = async (newEvent: Event) => {
+        const eventsData = data || []
+
+        try {
+            await mutate([...eventsData, newEvent], {
+                optimisticData: [...eventsData, newEvent],
+                rollbackOnError: true,
+                // Make the API request to add the new event here (You need to implement this)
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    return { data, addEvent }
 }
 
 export default useEvents
