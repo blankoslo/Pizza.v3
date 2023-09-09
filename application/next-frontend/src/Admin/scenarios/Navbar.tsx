9266ab263b8f1fa373f44d5c7c726c41e7f91914
stianjsu <stianjsu@gmail.com>
2023-09-09 20:53:41 +0200

    PB-17h: adds apiHelpermethod for log out request

--- application/next-frontend/src/Admin/scenarios/Navbar.tsx
+++ application/next-frontend/src/Admin/scenarios/Navbar.tsx
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router'
 import { User } from '../types/User'
+import { apiRequestHelper } from '@/api/utils'
 
 const Navbar = ({ user }: { user: User }) => {
     const router = useRouter()
 
     const logout = async () => {
-        const res = await fetch('/auth/logout') // TODO: change to apihelper delete method
-
-        if (res.ok) router.push('/login')
+        try {
+            const res = await apiRequestHelper.del<{ msg: string }>('/auth/logout')
+            if (res) router.push('/login')
+        } catch (err) {
+            console.error(err)
+        }
     }
     return (
         <div className="flex w-full items-center gap-4 overflow-hidden bg-teal-400 px-8 py-4 text-gray-700">
