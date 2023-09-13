b6c125b7343e088340c2de41b16420060ff5a6ac
stianjsu <stianjsu@gmail.com>
2023-09-13 15:18:23 +0200

    Refactored SlackUsers component

--- application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
+++ application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
@@ -5,35 +5,25 @@ import { Switch } from '@mui/material'
 const SlackUsers = () => {
     const { data, isLoading, error, updateUser } = useSlackUsers()
 
-    if (isLoading) {
-        return <CardComponent title="People">Loading...</CardComponent>
-    }
-
-    if (error) {
-        return (
-            <CardComponent title="People">
-                Failed to load users due to the following error: {error?.info.msg}
-            </CardComponent>
-        )
-    }
-
-    if (!data || data.length == 0) {
-        return <CardComponent title="People">No users found.</CardComponent>
-    }
-
     return (
         <CardComponent title="People">
-            {data.map((slackUser) => (
-                <div key={slackUser.slack_id} className="flex items-center justify-between py-2">
-                    <Switch
-                        checked={slackUser.active}
-                        onChange={() => updateUser(slackUser)}
-                        color="success"
-                        inputProps={{ 'aria-label': 'controlled' }}
-                    />
-                    <p className="ml-3">{slackUser.current_username}</p>
-                </div>
-            ))}
+            {isLoading
+                ? 'Loading...'
+                : error
+                ? `Failed to load users due to the following error: ${error?.info.msg}`
+                : !data || data.length == 0
+                ? 'No users found.'
+                : data.map((slackUser) => (
+                      <div key={slackUser.slack_id} className="flex items-center justify-between py-2">
+                          <Switch
+                              checked={slackUser.active}
+                              onChange={() => updateUser(slackUser)}
+                              color="success"
+                              inputProps={{ 'aria-label': 'controlled' }}
+                          />
+                          <p className="ml-3">{slackUser.current_username}</p>
+                      </div>
+                  ))}
         </CardComponent>
     )
 }
