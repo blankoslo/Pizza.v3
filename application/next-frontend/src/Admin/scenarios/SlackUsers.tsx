2c9eba69001adccd8d3ab4e3bbf37117da4c8fc5
Stian Sulebak <69514522+stianjsu@users.noreply.github.com>
2023-09-14 11:18:56 +0200

    PB-30b: implements toggling active on workspace users (#31)
    * refactor: cleaned up code and mutation implementation
    
    * refactor: explicitly set mutator options
    
    * PB-30b: implements toggling active on slackusers
    
    * PB-30b: adds mui switch
    
    * PB-30b: revamped admin page styling for better responsiveness
    
    * removes console log
    
    * Refactored SlackUsers component

--- application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
+++ application/next-frontend/src/Admin/scenarios/SlackUsers.tsx
@@ -1,42 +1,29 @@
 import { CardComponent } from 'Admin/components/CardComponent'
-import useSlackUsers from '@/api/useSlackUsers'
+import { useSlackUsers } from '@/api/useSlackUsers'
+import { Switch } from '@mui/material'
 
 const SlackUsers = () => {
-    const { data, isLoading, error } = useSlackUsers()
-
-    if (isLoading) {
-        return (
-            <CardComponent title="People" className="w-1/4">
-                Loading...
-            </CardComponent>
-        )
-    }
-
-    if (error) {
-        return (
-            <CardComponent title="People" className="w-1/4">
-                Failed to load users due to the following error: {error?.info.msg}
-            </CardComponent>
-        )
-    }
-
-    if (!data || data.length == 0) {
-        return (
-            <CardComponent title="People" className="w-1/4">
-                No users found.
-            </CardComponent>
-        )
-    }
+    const { data, isLoading, error, updateUser } = useSlackUsers()
 
     return (
-        <CardComponent title="People" className="w-1/4">
-            {data.map((slackUser) => (
-                <div key={slackUser.slack_id} className="flex items-center justify-between py-2">
-                    <div className="h-10 w-10 overflow-hidden rounded-full border border-black"></div>
-                    <p className="ml-3">{slackUser.current_username}</p>
-                    <button className="ml-auto">&times;</button>
-                </div>
-            ))}
+        <CardComponent title="People">
+            {isLoading
+                ? 'Loading...'
+                : error
+                ? `Failed to load users due to the following error: ${error?.info.msg}`
+                : !data || data.length == 0
+                ? 'No users found.'
+                : data.map((slackUser) => (
+                      <div key={slackUser.slack_id} className="flex items-center justify-between py-2">
+                          <Switch
+                              checked={slackUser.active}
+                              onChange={() => updateUser(slackUser)}
+                              color="success"
+                              inputProps={{ 'aria-label': 'controlled' }}
+                          />
+                          <p className="ml-3">{slackUser.current_username}</p>
+                      </div>
+                  ))}
         </CardComponent>
     )
 }
