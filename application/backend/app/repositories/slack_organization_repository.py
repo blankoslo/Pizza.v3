5827f5f43e4b05e228fa412f5a55e259c4449632
stianjsu <stianjsu@gmail.com>
2023-09-15 17:11:58 +0200

    PB-16: query scheduled events for an organization

--- application/backend/app/repositories/slack_organization_repository.py
+++ application/backend/app/repositories/slack_organization_repository.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+
+from app.db import db
+
 from app.models.slack_organization import SlackOrganization
+from app.models.event import Event
 from app.models.mixins import CrudMixin
 
-
 class SlackOrganizationRepository(SlackOrganization, CrudMixin):
-    pass
+    @classmethod
+    def get_scheduled_events(cls, team_id = None, session=db.session):
+        query = session.query(cls.team_id, Event.id, Event.time) \
+            .outerjoin(
+                Event,
+                cls.team_id == Event.slack_organization_id
+            ) \
+            .filter(
+                team_id == cls.team_id if team_id is not None else True,
+                Event.time > datetime.now()
+            )
+
+        res = query.count(), query.all()
+        return res
