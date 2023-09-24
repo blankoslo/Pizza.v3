272e1c41bd0cb20d29da57b3d7d4d849a327f989
Sergey <thefanthomasfan@gmail.com>
2023-09-24 17:56:42 +0200

    PB-39a: Updated configs to allow use of svgs as components, imported svg of mascot, and created file where it will reside

---
+++ application/next-frontend/next.config.mjs
@@ -0,0 +1,10 @@
+import withSvgr from 'next-svgr'
+
+/**
+ * @type {import('next').NextConfig}
+ * @type {import('next-svgr').withSvgr}
+ */
+
+const nextConfig = { reactStrictMode: true }
+
+export default withSvgr(nextConfig)
---
+++ application/next-frontend/src/Admin/components/AdminSidebar.tsx
@@ -0,0 +1,18 @@
+// import { ReactComponent as Mascot } from 'Admin/assets/Mascot.svg'
+
+const AdminSidebar = () => {
+    return (
+        <div>
+            <p>Welcome to the Admin Page!</p>
+            <p>
+                As the administrator, you can edit who is invited, where you are going and when the events take place.
+            </p>
+            <div>
+                {/* rotate 4 or 5 degrees to the left */}
+                {/* <Mascot /> */}
+            </div>
+        </div>
+    )
+}
+
+export default AdminSidebar
