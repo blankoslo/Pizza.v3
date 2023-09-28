5fd23a47e1e8b68d0fe398eb1f8b4e3a6c840e12
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2023-09-28 13:21:54 +0200

    Implemented new design for admin panel (#39)
    * PB-39a: Updated configs to allow use of svgs as components, imported svg of mascot, and created file where it will reside
    
    * PB-39b: Added custom style to tailwind config, made progress on new style for card components
    
    * PB-39a: Divided the screen into two parts, one showing the mascot and the other showing restaurants etc
    
    * renamed file
    
    * PB-39: Worked on feedback from Stian, added font and svg of background
    
    * reverted package-lock.json

---
+++ application/next-frontend/src/Admin/components/AdminSidebar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Mascot from 'Admin/assets/Mascot.svg'
+import TextBackground from 'Admin/assets/AdminSidebarBackground.svg'
+
+const AdminSidebar = () => {
+    return (
+        <div>
+            <div className="font-workSans text-white">
+                <div className="relative mx-auto mb-8 w-[22rem]">
+                    <Image priority src={TextBackground} width={345} alt="background" />
+                    <h1 className="absolute left-[57%] top-[54%] w-[22rem] -translate-x-1/2 -translate-y-1/2 font-queensCTM text-6xl text-[#05793C]">
+                        Pizza control in progress..
+                    </h1>
+                </div>
+
+                <div className="mx-auto w-[19rem]">
+                    <p>Welcome to the Admin Page!</p>
+                    <p>
+                        As the administrator, you can edit who is invited, where you are going and when the events take
+                        place.
+                    </p>
+                </div>
+            </div>
+
+            <div className="mx-auto" style={{ border: '2px solid yellow' }}>
+                <Image priority src={Mascot} width={450} alt="mascot" />
+            </div>
+        </div>
+    )
+}
+
+export { AdminSidebar }
