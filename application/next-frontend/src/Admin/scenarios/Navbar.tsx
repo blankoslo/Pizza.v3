476e7e7f461f081ac94904ccd2cadbca8fba145c
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2023-09-29 10:40:09 +0200

    Implemented new navbar design (#44)
    PB-39j: Implemented new navbar design
    
    Co-authored-by: Sergey <sergey@Joshuas-MBP.localdomain>

--- application/next-frontend/src/Admin/scenarios/Navbar.tsx
+++ application/next-frontend/src/Admin/scenarios/Navbar.tsx
@@ -1,9 +1,12 @@
 import { useRouter } from 'next/router'
-import { User } from '../types/User'
 import { apiRequestHelper } from '@/api/utils'
+import { useState } from 'react'
+import Image from 'next/image'
+import PizzaBotLogo from 'Admin/assets/PizzaBotLogo.svg'
 
-const Navbar = ({ user }: { user: User }) => {
+const Navbar = () => {
     const router = useRouter()
+    const [markedIndex, setMarkedIndex] = useState(2)
 
     const logout = async () => {
         try {
@@ -13,11 +16,40 @@ const Navbar = ({ user }: { user: User }) => {
             console.error(err)
         }
     }
+
+    const navbarEntries = [
+        {
+            text: 'Help',
+            key: 0,
+        },
+        {
+            text: 'Policy',
+            key: 1,
+        },
+        {
+            text: 'Admin',
+            key: 2,
+        },
+    ]
+
     return (
-        <div className="flex w-full items-center gap-4 overflow-hidden bg-teal-400 px-8 py-4 text-gray-700">
-            <div className="flex-1">Logged in as {user.name}</div>
-            <button className="m-auto w-fit border border-black px-8 py-2 hover:bg-slate-300" onClick={logout}>
-                Logout
+        <div className="flex w-full items-center gap-4 overflow-hidden bg-white py-4 font-workSans text-xl text-[#303030]">
+            <div className="ml-16 flex-1">
+                <Image priority src={PizzaBotLogo} width={150} alt="pizza bot logo" />
+            </div>
+            {navbarEntries.map((entry) => (
+                <button
+                    key={entry.key}
+                    className={`mr-10 hover:font-bold hover:text-[#05793C] ${
+                        entry.key == markedIndex ? 'marked-entry font-bold text-[#05793C]' : ''
+                    }`}
+                    onClick={() => setMarkedIndex(entry.key)}
+                >
+                    {entry.text}
+                </button>
+            ))}
+            <button className="mr-10 hover:font-bold hover:text-[#05793C]" onClick={() => logout()}>
+                Log out
             </button>
         </div>
     )
