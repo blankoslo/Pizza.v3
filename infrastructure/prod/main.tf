4f3c2ea4a8ff1aa4aeb12ddfea4a665c5f97856c
Jacob Theisen <55831903+JacobTheisen@users.noreply.github.com>
2023-09-29 14:34:48 +0200

    push current main to prod. (#47)
    * Create staging (#41)
    
    * PB-27n: force https
    
    * PB-27N: enforce https by returning 301 on http
    
    * PB-27n: rename
    
    * PB-27n: bugfix
    
    * PB-41: Change layout of /infrastructure for several terraform workspaces
    
    * Create staging (#42)
    
    * PB-27n: force https
    
    * PB-27N: enforce https by returning 301 on http
    
    * PB-27n: rename
    
    * PB-27n: bugfix
    
    * PB-41: Change layout of /infrastructure for several terraform workspaces
    
    * PB-41:bugfix
    
    * Create staging (#43)
    
    * PB-27n: force https
    
    * PB-27N: enforce https by returning 301 on http
    
    * PB-27n: rename
    
    * PB-27n: bugfix
    
    * PB-41: Change layout of /infrastructure for several terraform workspaces
    
    * PB-41:bugfix
    
    * Create staging (#45)
    
    * PB-27n: force https
    
    * PB-27N: enforce https by returning 301 on http
    
    * PB-27n: rename
    
    * PB-27n: bugfix
    
    * PB-41: Change layout of /infrastructure for several terraform workspaces
    
    * PB-41:bugfix
    
    * PB-41: bugfix
    
    * Implemented new navbar design (#44)
    
    PB-39j: Implemented new navbar design
    
    Co-authored-by: Sergey <sergey@Joshuas-MBP.localdomain>
    
    * Create staging (#46)
    
    * PB-27n: force https
    
    * PB-27N: enforce https by returning 301 on http
    
    * PB-27n: rename
    
    * PB-27n: bugfix
    
    * PB-41: Change layout of /infrastructure for several terraform workspaces
    
    * PB-41:bugfix
    
    * PB-41: bugfix
    
    * PB-41: Hvordan snek den her seg inn i pren?
    
    ---------
    
    Co-authored-by: Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
    Co-authored-by: Sergey <sergey@Joshuas-MBP.localdomain>

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -66,59 +66,60 @@ resource "heroku_pipeline_coupling" "production-frontend" {
   stage = "production"
 }
 
-/*
-module "staging" {
-  source = "./system"
 
-  heroku_team_name = var.heroku_team_name
-  hostname = "staging.bot.blank.pizza"
-  prefix = var.prefix
-  environment = "stag"
-  CLOUDAMQP_PLAN = "cloudamqp:lemur"
-  PAPERTRAIL_PLAN = "papertrail:choklad"
-  POSTGRES_PLAN = "heroku-postgresql:mini"
-  FORMATION_SIZE_FRONTEND = "Basic"
-  FORMATION_SIZE_BACKEND = "Basic"
-  FORMATION_SIZE_BOT_WORKER = "Basic"
-  FORMATION_QUANTITY_FRONTEND = 1
-  FORMATION_QUANTITY_BACKEND = 1
-  FORMATION_QUANTITY_BOT_WORKER = 1
-  SLACK_APP_TOKEN = var.STAGING_SLACK_APP_TOKEN
-  SLACK_CLIENT_ID: var.STAGING_SLACK_CLIENT_ID
-  SLACK_CLIENT_SECRET: var.STAGING_SLACK_CLIENT_SECRET
-  SLACK_SIGNING_SECRET: var.STAGING_SLACK_SIGNING_SECRET
-  SECRET_KEY_BACKEND = var.STAGING_SECRET_KEY_BACKEND
-  CLOUDINARY_CLOUD_NAME  = var.STAGING_CLOUDINARY_CLOUD_NAME
-  CLOUDINARY_API_KEY  = var.STAGING_CLOUDINARY_API_KEY
-  CLOUDINARY_API_SECRET  = var.STAGING_CLOUDINARY_API_SECRET
-  MQ_EVENT_KEY = "pizza"
-  MQ_EVENT_QUEUE = "Pizza_Queue"
-  MQ_EXCHANGE = "Pizza_Exchange"
-  MQ_RPC_KEY = "rpc"
-  DAYS_IN_ADVANCE_TO_INVITE = 10
-  HOURS_BETWEEN_REMINDERS = 4
-  REPLY_DEADLINE_IN_HOURS = 24
-  FLASK_ENV = "production"
-  BACKEND_URI = "staging.api.bot.blank.pizza"
-  FRONTEND_URI = "staging.bot.blank.pizza"
-}
+# module "staging" {
+#   source = "./system"
 
-# Add staging apps to pipeline under staging stage
-resource "heroku_pipeline_coupling" "staging-backend" {
-  app_id = module.staging.app_backend_id
-  pipeline = heroku_pipeline.pizzabot.id
-  stage = "staging"
-}
+#   heroku_team_name = var.heroku_team_name
+#   hostname = "staging.pizzabot.app"
+#   prefix = var.prefix
+#   environment = "stag"
+#   CLOUDAMQP_PLAN = "cloudamqp:lemur"
+#   PAPERTRAIL_PLAN = "papertrail:choklad"
+#   POSTGRES_PLAN = "heroku-postgresql:mini"
+#   FORMATION_SIZE_FRONTEND = "Basic"
+#   FORMATION_SIZE_BACKEND = "Basic"
+#   FORMATION_SIZE_BOT_WORKER = "Basic"
+#   FORMATION_QUANTITY_FRONTEND = 1
+#   FORMATION_QUANTITY_BACKEND = 1
+#   FORMATION_QUANTITY_BOT_WORKER = 1
+#   SLACK_APP_TOKEN = var.STAGING_SLACK_APP_TOKEN
+#   SLACK_CLIENT_ID: var.STAGING_SLACK_CLIENT_ID
+#   SLACK_CLIENT_SECRET: var.STAGING_SLACK_CLIENT_SECRET
+#   SLACK_SIGNING_SECRET: var.STAGING_SLACK_SIGNING_SECRET
+#   SECRET_KEY_BACKEND = var.STAGING_SECRET_KEY_BACKEND
+#   CLOUDINARY_CLOUD_NAME  = var.STAGING_CLOUDINARY_CLOUD_NAME
+#   CLOUDINARY_API_KEY  = var.STAGING_CLOUDINARY_API_KEY
+#   CLOUDINARY_API_SECRET  = var.STAGING_CLOUDINARY_API_SECRET
+#   MQ_EVENT_KEY = "pizza"
+#   MQ_EVENT_QUEUE = "Pizza_Queue"
+#   MQ_EXCHANGE = "Pizza_Exchange"
+#   MQ_RPC_KEY = "rpc"
+#   DAYS_IN_ADVANCE_TO_INVITE = 10
+#   HOURS_BETWEEN_REMINDERS = 4
+#   REPLY_DEADLINE_IN_HOURS = 24
+#   FLASK_ENV = "production"
+#   BACKEND_URI = "api.staging.pizzabot.app"
+#   NEXT_PUBLIC_BACKEND_URI = "https://api.staging.pizzabot.app"
+#   FRONTEND_URI = "staging.pizzabot.app"
+# }
 
-resource "heroku_pipeline_coupling" "staging-bot" {
-  app_id = module.staging.app_bot_id
-  pipeline = heroku_pipeline.pizzabot.id
-  stage = "staging"
-}
+# #Add staging apps to pipeline under staging stage
+# resource "heroku_pipeline_coupling" "staging-backend" {
+#   app_id = module.staging.app_backend_id
+#   pipeline = heroku_pipeline.pizzabot.id
+#   stage = "staging"
+# }
 
-resource "heroku_pipeline_coupling" "staging-frontend" {
-  app_id = module.staging.app_frontend_id
-  pipeline = heroku_pipeline.pizzabot.id
-  stage = "staging"
-}
-*/
+# resource "heroku_pipeline_coupling" "staging-bot" {
+#   app_id = module.staging.app_bot_id
+#   pipeline = heroku_pipeline.pizzabot.id
+#   stage = "staging"
+# }
+
+# resource "heroku_pipeline_coupling" "staging-frontend" {
+#   app_id = module.staging.app_frontend_id
+#   pipeline = heroku_pipeline.pizzabot.id
+#   stage = "staging"
+# }
+
--- infrastructure/staging/variables.tf
+++ infrastructure/staging/variables.tf
@@ -21,40 +21,40 @@ variable "heroku_team_name" {
 }
 
 # ************* PRODUCTION ************* #
-variable "PRODUCTION_SLACK_APP_TOKEN" {
-  type = string
-}
+# variable "PRODUCTION_SLACK_APP_TOKEN" {
+#   type = string
+# }
 
-variable "PRODUCTION_SLACK_CLIENT_ID" {
-  type = string
-}
+# variable "PRODUCTION_SLACK_CLIENT_ID" {
+#   type = string
+# }
 
-variable "PRODUCTION_SLACK_CLIENT_SECRET" {
-  type = string
-}
+# variable "PRODUCTION_SLACK_CLIENT_SECRET" {
+#   type = string
+# }
 
-variable "PRODUCTION_SLACK_SIGNING_SECRET" {
-  type = string
-}
+# variable "PRODUCTION_SLACK_SIGNING_SECRET" {
+#   type = string
+# }
 
-variable "PRODUCTION_SECRET_KEY_BACKEND" {
-  type = string
-}
+# variable "PRODUCTION_SECRET_KEY_BACKEND" {
+#   type = string
+# }
 
-variable "PRODUCTION_CLOUDINARY_CLOUD_NAME" {
-  type = string
-}
+# variable "PRODUCTION_CLOUDINARY_CLOUD_NAME" {
+#   type = string
+# }
 
-variable "PRODUCTION_CLOUDINARY_API_KEY" {
-  type = string
-}
+# variable "PRODUCTION_CLOUDINARY_API_KEY" {
+#   type = string
+# }
 
-variable "PRODUCTION_CLOUDINARY_API_SECRET" {
-  type = string
-}
+# variable "PRODUCTION_CLOUDINARY_API_SECRET" {
+#   type = string
+# }
 
 # ************* STAGING ************* #
-/*
+
 variable "STAGING_SLACK_APP_TOKEN" {
   type = string
 }
@@ -86,4 +86,9 @@ variable "STAGING_CLOUDINARY_API_KEY" {
 variable "STAGING_CLOUDINARY_API_SECRET" {
   type = string
 }
-*/
+
+variable "EXISTING_PIPELINE_ID" {
+  type = string
+  description = "As the apps pushed to production is already created under an existing pipeline, we have to reference this pipeline so we dont have to create another"
+  default = "deb1e6a1-f075-4153-a818-b39fd0351140"
+}
