ba099b59a4ac1f7970eebafa363ba3968806db52
Sergey <sergey@Joshuas-MBP.localdomain>
2023-10-04 14:29:20 +0200

    Almost there

--- application/next-frontend/src/Admin/scenarios/Events/components/EventCalendar.tsx
+++ application/next-frontend/src/Admin/scenarios/Events/components/EventCalendar.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import Image from 'next/image'
+import Triangle from 'Admin/assets/Triangle.svg'
+import Line from 'Admin/assets/Line.svg'
 
 // Goes into the next month - 1 day, which is the last day of current month, and returns this day
 // e.g. if we are in October, and our last day is 31st, we return 31
@@ -31,6 +34,7 @@ const EventCalendar = () => {
         'November',
         'December',
     ]
+    const today = new Date()
 
     const setPreviousMonth = () => {
         setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1))
@@ -61,37 +65,66 @@ const EventCalendar = () => {
                     .map((_, dayOfWeek) => {
                         if (startIndex <= dayOfWeek && dayOfWeek <= endIndex) {
                             const day = remainingDays.pop()
+                            const selectedDate = new Date(
+                                currentDate.getFullYear(),
+                                currentDate.getMonth(),
+                                day ? day + 1 : day,
+                            )
                             return (
                                 <td
-                                    className="h-16 w-16 border border-gray-300 text-center align-top"
+                                    className={`h-[40px] w-[43px] border border-[#05793C] text-center align-top text-[#303030] ${
+                                        selectedDate > today ? 'cursor-pointer bg-white' : 'opacity-50'
+                                    }`}
                                     key={dayOfWeek}
-                                    onClick={() => onClickDay(day ?? 0)}
+                                    onClick={selectedDate > today ? () => onClickDay(day ?? 0) : () => ''}
                                 >
                                     {day}
                                 </td>
                             )
                         }
-                        return <td className="h-16 w-16 border border-gray-300" key={dayOfWeek} />
+                        return <td className="h-[40px] w-[43px] border border-[#05793C]" key={dayOfWeek} />
                     })}
             </tr>
         )
     }
 
     const onClickDay = (day: number) => {
-        alert(`You clicked on ${day}. Also check out ${currentDate}!`)
+        alert(`You clicked on ${new Date(currentDate.getFullYear(), currentDate.getMonth(), day)}!`)
     }
 
     return (
-        <div>
-            <button onClick={() => setPreviousMonth()}>Previous</button>
-            <span>{months[currentDate.getMonth()]}</span>
-            <button onClick={() => setNextMonth()}>Next</button>
-            <div className="mx-auto flex w-[80%] justify-center border border-dashed border-[#05793C]" />
-            <table>
+        <div className="inline-block bg-[#EDFFF6] p-8 text-[#05793C]">
+            <div className="flex justify-center">
+                <Image
+                    onClick={() => setPreviousMonth()}
+                    width={15}
+                    src={Triangle}
+                    className="rotate-180 cursor-pointer"
+                    alt="show previous month"
+                />
+                <span className="mx-3 font-queensCTM text-4xl">{months[currentDate.getMonth()]}</span>
+                <Image
+                    onClick={() => setNextMonth()}
+                    width={15}
+                    src={Triangle}
+                    alt="show next month"
+                    className="cursor-pointer"
+                />
+            </div>
+            <Image
+                src={Line}
+                className="mx-auto mb-10 mt-4"
+                width={440}
+                alt="line separating calendar and month display"
+            />
+            {/* <div className="mx-auto flex  mt-5 mb-14 justify-center border border-dashed border-[#05793C]" /> */}
+            <table className="mx-auto italic">
                 <thead>
                     <tr>
                         {weekdays.map((day) => (
-                            <th key={day}>{day}</th>
+                            <th className="font-workSans text-xl font-black text-[#05793C]" key={day}>
+                                {day}
+                            </th>
                         ))}
                     </tr>
                 </thead>
