6b19d9f076f78fbd74117bf1caba7c7ac4841a3b
Anna <annabjorgo0308@gmail.com>
2023-10-10 14:37:10 +0200

    create index file for onboarding

--- application/next-frontend/pages/admin/onboarding/index.tsx
+++ application/next-frontend/pages/admin/onboarding/index.tsx
@@ -2,13 +2,14 @@ import { GetServerSideProps } from 'next'
 import jwtDecode from 'jwt-decode'
 import type { JwtToken, User } from '@/Admin/types/User'
 import { Navbar } from '@/Admin/scenarios/Navbar'
-import { Onboarding as OnboardAdmin} from '@/Admin/scenarios/Onboarding'
+import { Onboarding as OnboardAdmin } from '@/Admin/scenarios/Onboarding'
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const jwt = req.cookies['access_token_cookie']
     if (!jwt) return { redirect: { destination: '/login', permanent: false }, props: {} }
 
     const decodedJWT = jwtDecode<JwtToken>(jwt)
+
     return {
         props: {
             user: decodedJWT.user,
@@ -20,7 +21,7 @@ const Onboarding = ({ user }: { user: User }) => {
     return (
         <div>
             <Navbar user={user} />
-            <OnboardAdmin/>
+            <OnboardAdmin />
         </div>
     )
 }
