c9c682a390f7eab556e8cd922d5a150b10d52960
Sergey <sergey@Joshuas-MBP.localdomain>
2023-10-11 17:22:38 +0200

    added them to main page

--- application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
+++ application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
@@ -1,26 +1,34 @@
-// import { Events } from 'Admin/scenarios/Events'
-// import { Restaurants } from 'Admin/scenarios/Restaurants'
-// import { SlackUsers } from 'Admin/scenarios/SlackUsers'
-// import Image from 'next/image'
-// import MascotHappy from 'Admin/assets/MascotHappy.svg'
+import { Events } from 'Admin/scenarios/Events'
+import { Restaurants } from 'Admin/scenarios/Restaurants'
+import { SlackUsers } from 'Admin/scenarios/SlackUsers'
+import Image from 'next/image'
+import MascotHappy from 'Admin/assets/MascotHappy.svg'
+import { CreatePizzaEventCard } from './Events/components/CreatePizzaEventCard'
 import { DeletePizzaEventCard } from './Events/components/DeletePizzaEventCard'
 
 const AdminMainPage = () => {
     return (
         <div>
             <DeletePizzaEventCard />
+            <CreatePizzaEventCard />
+            <div className="grid-background flex w-full justify-evenly pt-16">
+                <div className="w-1/3 p-4 [&>*]:mb-14">
+                    <h1 className="font-queensMedium text-6xl">Pizza Admin</h1>
+                    <Events />
+                    <SlackUsers />
+                </div>
+                <div className="w-1/3 p-4 [&>*]:mb-14">
+                    <Image
+                        className="mt-16"
+                        priority
+                        src={MascotHappy}
+                        width={450}
+                        alt="pizza bot mascot with four arms"
+                    />
+                    <Restaurants />
+                </div>
+            </div>
         </div>
-        // <div className="grid-background flex w-full justify-evenly pt-16">
-        //     <div className="w-1/3 p-4 [&>*]:mb-14">
-        //         <h1 className="font-queensMedium text-6xl">Pizza Admin</h1>
-        //         <Events />
-        //         <SlackUsers />
-        //     </div>
-        //     <div className="w-1/3 p-4 [&>*]:mb-14">
-        //         <Image className="mt-16" priority src={MascotHappy} width={450} alt="pizza bot mascot with four arms" />
-        //         <Restaurants />
-        //     </div>
-        // </div>
     )
 }
 
