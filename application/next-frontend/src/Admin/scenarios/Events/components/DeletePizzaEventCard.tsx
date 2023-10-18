d06474f32170c5dbd4046ea60ad177292f57b874
Sergey <sergey@MacBook-Pro.localdomain>
2023-10-18 13:54:25 +0200

    PB-39k: added api integration to 'delete event'-card

--- application/next-frontend/src/Admin/scenarios/Events/components/DeletePizzaEventCard.tsx
+++ application/next-frontend/src/Admin/scenarios/Events/components/DeletePizzaEventCard.tsx
@@ -1,3 +1,5 @@
+import { ApiEvent, useEvents } from '@/api/useEvents'
+
 const addOrdinalToDate = (date: string) => {
     if (date.length > 1 && date.slice(-2) === '1') return date + 'th'
     else
@@ -13,8 +15,8 @@ const addOrdinalToDate = (date: string) => {
         }
 }
 
-const DeletePizzaEventCard = () => {
-    const today = new Date()
+const DeletePizzaEventCard = (event: ApiEvent) => {
+    const eventDate = new Date(event.time)
     const months = [
         'January',
         'February',
@@ -29,7 +31,12 @@ const DeletePizzaEventCard = () => {
         'November',
         'December',
     ]
-    const [date, month, time] = [today.getDate(), today.getMonth(), '6:00']
+    const [date, month, time] = [
+        eventDate.getDate(),
+        eventDate.getMonth(),
+        `${eventDate.getHours()}:${eventDate.getMinutes()}`,
+    ]
+    const { delEvent } = useEvents()
 
     return (
         <div className="h-[448px] w-[703px] rounded-[32px] border bg-[#EDFFF6] px-14 py-10">
@@ -51,7 +58,10 @@ const DeletePizzaEventCard = () => {
             </div>
 
             <div className="mt-14 flex justify-evenly">
-                <button className="my-4 w-[270px] border-2 border-[#4E5445] py-4 font-workSans text-2xl font-semibold text-[#4E5445] hover:bg-[#FFF8C1] focus:outline-none">
+                <button
+                    className="my-4 w-[270px] border-2 border-[#4E5445] py-4 font-workSans text-2xl font-semibold text-[#4E5445] hover:bg-[#FFF8C1] focus:outline-none"
+                    onClick={() => delEvent(event.id)}
+                >
                     Cancel
                 </button>
                 <button className="my-4 w-[270px] border-2 border-b-8 border-[#2D8F5C] bg-white py-4 font-workSans text-2xl font-black text-[#2D8F5C] hover:bg-[#FFB9B9] focus:outline-none">
