ff1ee91fa65bc72bdd5486f51b567b303a67dfce
Sergey <sergey@MacBook-Pro.localdomain>
2023-10-18 16:58:57 +0200

    integrated api to new cards

--- application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
+++ application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
@@ -3,12 +3,10 @@ import { Restaurants } from 'Admin/scenarios/Restaurants'
 import { SlackUsers } from 'Admin/scenarios/SlackUsers'
 import Image from 'next/image'
 import MascotHappy from 'Admin/assets/MascotHappy.svg'
-import { CreatePizzaEventCard } from './Events/components/CreatePizzaEventCard'
 
 const AdminMainPage = () => {
     return (
         <div>
-            <CreatePizzaEventCard />
             <div className="grid-background flex w-full justify-evenly pt-16">
                 <div className="w-1/3 p-4 [&>*]:mb-14">
                     <h1 className="font-queensMedium text-6xl">Pizza Admin</h1>
