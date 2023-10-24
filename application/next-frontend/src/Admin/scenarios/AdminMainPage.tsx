655f76b9bf0b753af40228a42bb98d767b26e156
Sergey <sergey@MacBook-Pro.localdomain>
2023-10-24 15:39:55 +0200

    PB-39n: Added closing tag

--- application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
+++ application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
@@ -19,30 +19,37 @@ const AdminMainPage = () => {
                     <CreatePizzaEventCard clickedDate={new Date()} />
                 </TestTemporary>
             </ModalProvider> */}
-        <div className="grid-background flex w-full justify-evenly pt-16">
-            <div className="w-1/3 p-4 [&>*]:mb-14">
-                <h1 className="font-queensMedium text-6xl">Pizza Admin</h1>
-                {calendarShowing ? (
-                    <div className="flex">
-                        <div className="grow" onClick={() => setCalendarShowing(false)}>
+            <div className="grid-background flex w-full justify-evenly pt-16">
+                <div className="w-1/3 p-4 [&>*]:mb-14">
+                    <h1 className="font-queensMedium text-6xl">Pizza Admin</h1>
+                    {calendarShowing ? (
+                        <div className="flex">
+                            <div className="grow" onClick={() => setCalendarShowing(false)}>
+                                <Events />
+                            </div>
+                            <div className="z-10 flex-none">
+                                <EventCalendar />
+                            </div>
+                        </div>
+                    ) : (
+                        <div onClick={() => setCalendarShowing(true)}>
                             <Events />
                         </div>
-                        <div className="z-10 flex-none">
-                            <EventCalendar />
-                        </div>
+                    )}
+                    <div className={`${calendarShowing ? 'opacity-25' : ''} `}>
+                        <SlackUsers />
                     </div>
-                ) : (
-                    <div onClick={() => setCalendarShowing(true)}>
-                        <Events />
-                    </div>
-                )}
-                <div className={`${calendarShowing ? 'opacity-25' : ''} `}>
-                    <SlackUsers />
                 </div>
-            </div>
-            <div className={`z-0 w-1/3 p-4 [&>*]:mb-14 ${calendarShowing ? 'opacity-25' : ''} `}>
-                <Image className="mt-16" priority src={MascotHappy} width={450} alt="pizza bot mascot with four arms" />
-                <Restaurants />
+                <div className={`z-0 w-1/3 p-4 [&>*]:mb-14 ${calendarShowing ? 'opacity-25' : ''} `}>
+                    <Image
+                        className="mt-16"
+                        priority
+                        src={MascotHappy}
+                        width={450}
+                        alt="pizza bot mascot with four arms"
+                    />
+                    <Restaurants />
+                </div>
             </div>
         </div>
     )
