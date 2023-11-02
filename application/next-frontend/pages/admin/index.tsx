755e5a8b51e31ac830cfbf97878007077a44e857
Stian Sulebak <69514522+stianjsu@users.noreply.github.com>
2023-11-02 17:44:39 +0100

    feat: adds redirect to onboarding if no resturants added (#69)

--- application/next-frontend/pages/admin/index.tsx
+++ application/next-frontend/pages/admin/index.tsx
@@ -3,10 +3,28 @@ import jwtDecode from 'jwt-decode'
 import type { JwtToken } from '@/Admin/types/User'
 import { AdminMainPage } from '@/Admin/scenarios/AdminMainPage'
 import { Navbar } from '@/Admin/scenarios/Navbar'
+import { serversideApiUri } from '@/api/endpoints'
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const jwt = req.cookies['access_token_cookie']
     if (!jwt || !jwtDecode<JwtToken>(jwt)) return { redirect: { destination: '/login', permanent: false }, props: {} }
+
+    try {
+        const resturantsRes = await fetch(`${serversideApiUri}/restaurants`, {
+            headers: {
+                cookie: req.headers.cookie,
+            } as HeadersInit,
+        })
+
+        if (resturantsRes.status !== 200) return { redirect: { destination: '/login', permanent: false }, props: {} }
+        const resturants = await resturantsRes.json()
+
+        // If no resturants is set up, redirect to onboarding
+        if (resturants.length === 0)
+            return { redirect: { destination: '/admin/onboarding', permanent: false }, props: {} }
+    } catch (error) {
+        return { redirect: { destination: '/login', permanent: false }, props: {} }
+    }
 
     return {
         props: {},
