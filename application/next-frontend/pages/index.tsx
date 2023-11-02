3896138d7be883d97ec030fa81a658324b430739
stianjsu <stianjsu@gmail.com>
2023-11-02 22:10:54 +0100

    fix: remove old landing page for user test

--- application/next-frontend/pages/index.tsx
+++ application/next-frontend/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Landing } from '@/Landing'
+/* import { Landing } from '@/Landing' */
 import { Header } from '@/Landing/scenarios/Header'
 
 export default function Home() {
     return (
         <div className="h-full">
             <Header />
-            <Landing />
+            <h1 className="mt-12 text-center font-workSans text-3xl">COMING SOON</h1>
+            {/* <Landing /> */}
         </div>
     )
 }
