f7392eea823a589319b91d02b34e45d9eea3bba8
sergiosja <sergey.jakobsen@gmail.com>
2023-11-09 15:09:41 +0100

    dry

--- application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
+++ application/next-frontend/src/Admin/scenarios/Restaurants/index.tsx
@@ -4,7 +4,6 @@ import { useRestaurants } from '@/api/useRestaurants'
 import { useModal } from 'Shared/context/ModalContext'
 import { useHover } from 'Shared/context/HoverContext'
 
-
 const Restaurants = () => {
     const { data, isLoading, error, delRestaurant } = useRestaurants()
     const { openModal } = useModal()
@@ -15,28 +14,28 @@ const Restaurants = () => {
     }
 
     return (
-            <CardComponentWrapper title="Places" editIcon onClickEditIcon={handleOnClickEditIcon}>
-                <div className="scrollable-wrapper mb-2 pb-8">
-                    <div className="scrollable-list">
-                        {isLoading
-                            ? 'Loading...'
-                            : error
-                            ? `Failed to load users due to the following error: ${error?.info.msg}`
-                            : !data || data.length == 0
-                            ? 'No restaurants found. Try adding one!'
-                            : (
-                                <div className="mt-5 flex flex-col pb-16">
-                                    <div className="pl-4 italic text-green-primary">You have added...</div>
-                                    <h3 className="text-2xl font-semibold leading-10">
-                                        <span className={`px-4 ${isHovered ? 'marked-entry-card' : ''}`}>
-                                            {data.length} {data.length == 1 ? 'Pizza Place' : 'Pizza Places'}
-                                        </span>
-                                    </h3>
-                                </div>
-                            )}
+        <CardComponentWrapper title="Places" editIcon onClickEditIcon={handleOnClickEditIcon}>
+            <div className="mb-2">
+                {isLoading ? (
+                    'Loading...'
+                ) : error ? (
+                    `Failed to load users due to the following error: ${error?.info.msg}`
+                ) : (
+                    <div className="mt-5 flex flex-col pb-16">
+                        <div className="pl-4 italic text-green-primary">
+                            {!data || data.length == 0 ? 'No restaurants found...' : 'You have added...'}
+                        </div>
+                        <h3 className="text-2xl font-semibold leading-10">
+                            <span className={`px-4 ${isHovered ? 'marked-entry-card' : ''}`}>
+                                {!data || data.length == 0
+                                    ? 'Try adding one!'
+                                    : `${data.length} ${data.length == 1 ? 'Pizza Place' : 'Pizza Places'}`}
+                            </span>
+                        </h3>
                     </div>
-                </div>
-            </CardComponentWrapper>
+                )}
+            </div>
+        </CardComponentWrapper>
     )
 }
 
