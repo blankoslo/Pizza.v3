ccae8353186a54de28a74900eaea054836b1c022
Stian Sulebak <69514522+stianjsu@users.noreply.github.com>
2023-11-09 13:55:30 +0100

    pb-39e: People card display channel name (#73)
    * feat: adds endpoint for getting current channel-name and users in the channel
    
    * feat: use channel user hook for connecting to api endpoint
    
    * feat: updates people card to new design
    
    * refactor: change slack req from post to get
    
    * chore: rename people-card to SlackChannel
    
    * chore: import inconsistency

--- application/backend/app/api/crud/slack_users.py
+++ application/backend/app/api/crud/slack_users.py
@@ -1,9 +1,11 @@
-from flask import views
+import requests
+from flask import jsonify, views
 from flask_smorest import Blueprint, abort
 from app.models.slack_user_schema import SlackUserResponseSchema, SlackUserUpdateSchema, SlackUserQueryArgsSchema
 from flask_jwt_extended import jwt_required, current_user
 from app.services.injector import injector
 from app.services.slack_user_service import SlackUserService
+from app.services.slack_organization_service import SlackOrganizationService
 
 bp = Blueprint("users", "users", url_prefix="/users", description="Operations on users")
 
@@ -19,6 +21,45 @@ class SlackUsers(views.MethodView):
         total, slack_users = slack_user_service.get(filters=args, page=pagination_parameters.page, per_page=pagination_parameters.page_size, order_by_ascending=True, team_id=current_user.slack_organization_id)
         pagination_parameters.item_count = total
         return slack_users
+    
+@bp.route("/current-channel")
+class SlackChannel(views.MethodView):
+    @jwt_required()
+    def get(self):
+        """Get current channel info"""
+        slack_organization = injector.get(SlackOrganizationService)
+        org = slack_organization.get_by_id(team_id=current_user.slack_organization_id)
+        channel_id = org.channel_id
+
+        if channel_id is None:
+            return jsonify({"channel_name": None, "users": [] })
+
+        # Request channel info from slack
+        request_url = f'https://slack.com/api/conversations.info?channel={channel_id}'
+        res = requests.get(request_url, headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {org.access_token}"
+        }).json()
+        
+        if not res["ok"]:
+            abort(500, message = "Error getting channel info")
+
+        channel_name = res["channel"]["name"]
+
+        slack_user_service = injector.get(SlackUserService)
+        count, slack_users = slack_user_service.get(filters={"slack_organization_id": current_user.slack_organization_id})
+        returned_users = [
+            {
+                "slack_id": user.slack_id,
+                "current_username": user.current_username,
+                "active": user.active,
+                "priority": user.priority,
+                "email": user.email,
+                "first_seen": user.first_seen,
+            } for user in slack_users
+        ]
+    
+        return jsonify({"channel_name": channel_name, "users": returned_users })
 
 @bp.route("/<slack_user_id>")
 class SlackUsersById(views.MethodView):
--- application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
+++ application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
@@ -1,11 +1,11 @@
 import { Events } from 'Admin/scenarios/Events'
 import { Restaurants } from 'Admin/scenarios/Restaurants'
-import { SlackUsers } from 'Admin/scenarios/SlackUsers'
+import { SlackChannel } from 'Admin/scenarios/SlackChannel'
 import Image from 'next/image'
 import MascotHappy from 'Admin/assets/MascotHappy.svg'
-import { EventCalendar } from './Events/components/EventCalendar'
+import { EventCalendar } from 'Admin/scenarios/Events/components/EventCalendar'
 import { useState, useRef, useEffect } from 'react'
-import { ModalProvider } from '../context/ModelContext'
+import { ModalProvider } from 'Admin/context/ModelContext'
 
 const AdminMainPage = () => {
     const [calendarShowing, setCalendarShowing] = useState(false)
@@ -51,7 +51,7 @@ const AdminMainPage = () => {
                         </div>
                     )}
                     <div className={`${calendarShowing ? 'opacity-25' : ''} `}>
-                        <SlackUsers />
+                        <SlackChannel />
                     </div>
                 </div>
                 <div className={`z-0 w-1/3 p-4 [&>*]:mb-14 ${calendarShowing ? 'opacity-25' : ''} `}>
