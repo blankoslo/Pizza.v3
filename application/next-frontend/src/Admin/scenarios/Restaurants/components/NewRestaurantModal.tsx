1746b1df12cf62f44e24b9e369fa30ba03f03d35
sergiosja <sergey.jakobsen@gmail.com>
2023-11-09 14:31:49 +0100

    updated colours to match custom palette

--- application/next-frontend/src/Admin/scenarios/Restaurants/components/NewRestaurantModal.tsx
+++ application/next-frontend/src/Admin/scenarios/Restaurants/components/NewRestaurantModal.tsx
@@ -17,64 +17,68 @@ const NewRestaurantModal = () => {
     const {
         register,
         handleSubmit,
+        setValue,
         formState: { errors },
     } = useForm({ resolver: zodResolver(validationSchema) })
 
     const { closeModal } = useModal()
-    const { addRestaurant } = useRestaurants()
+    const { data, addRestaurant, delRestaurant } = useRestaurants()
 
     const validateForm = (data: Restaurant) => {
-        const newRestaurant: Restaurant = {
-            name: data.name,
-        }
-        if (data.link !== '') {
-            newRestaurant.link = data.link
-        }
-        if (data.tlf !== '') {
-            newRestaurant.tlf = data.tlf
-        }
-        if (data.address !== '') {
-            newRestaurant.address = data.address
-        }
+        const newRestaurant: Restaurant = { name: data.name }
         addRestaurant(newRestaurant)
-        closeModal()
+        setValue('name', '')
     }
 
     return (
-        <div className="w-[25rem] bg-yellow p-4 shadow-2xl">
-            <div className="m-10 flex justify-between">
-                <h1 className="font-queensMedium text-5xl text-green-primary">New restaurant</h1>
+        <div className="relative w-[28rem] bg-yellow p-4">
+            <div className="flex justify-between text-green-primary">
+                <h5 className="mb-2 ml-6 pt-3 font-queensRegular text-3xl font-semibold">Places</h5>
+                <span className="cursor-pointer text-4xl" onClick={() => closeModal()}>
+                    &times;
+                </span>
             </div>
+            <div className="mx-auto flex w-[86%] justify-center border border-dashed border-green-primary" />
+            <div className="ml-3 px-4 py-3 text-dark">
+                <div className="mt-5 flex flex-col pb-6">
+                    <div className="pl-4 italic text-green-primary">You have added...</div>
+                    <h3 className="text-2xl font-semibold leading-10">
+                        <span className={`px-4`}>{data?.length || 0} Pizza Places</span>
+                    </h3>
+                </div>
 
-            <form onSubmit={handleSubmit((data) => validateForm(data as Restaurant))} className="flex flex-col px-6">
-                <input
-                    className="mb-4 border border-gray-800 bg-white p-3 font-workSans italic"
-                    {...register('name')}
-                    placeholder="Restaurant name*"
-                />
-                {errors.name && <p className=" font-workSans text-red-600"></p>}
-                <input
-                    className="mb-4 border border-gray-800 bg-white p-3 font-workSans italic"
-                    {...register('link')}
-                    placeholder="Restaurant link"
-                />
-                <input
-                    className="mb-4 border border-gray-800 bg-white p-3 font-workSans italic"
-                    {...register('tlf')}
-                    placeholder="Restaurant phone number"
-                />
-                <input
-                    className="mb-4 border border-gray-800 bg-white p-3 font-workSans italic"
-                    {...register('address')}
-                    placeholder="Restaurant address"
-                />
-                <Button
-                    type="submit"
-                    className="my-4 min-w-[7rem] text-xl hover:bg-green-secondary"
-                    text="Add"
-                    buttonStyle="primary"
-                />
-            </form>
+                <div className="flex flex-col justify-between">
+                    <form
+                        onSubmit={handleSubmit((data) => {
+                            validateForm(data as Restaurant)
+                        })}
+                        className="flex flex-col px-6"
+                    >
+                        <input
+                            className="mb-4 border border-gray-800 bg-white p-3 font-workSans italic hover:bg-green-secondary"
+                            {...register('name')}
+                            placeholder="Write your Pizza Places here"
+                        />
+                        {errors.name && <p></p>}
+                    </form>
+
+                    <div className="scrollable-wrapper mb-2 ">
+                        <div className="scrollable-list h-[525px]">
+                            {data &&
+                                data.map((restaurant) => (
+                                    <div
+                                        className="flex cursor-pointer px-6 py-1 text-2xl hover:text-red"
+                                        onClick={() => delRestaurant(restaurant.id)}
+                                        key={restaurant.id}
+                                    >
+                                        <button>&times;</button>
+                                        <p className="ml-2 font-workSans italic">{restaurant.name}</p>
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
