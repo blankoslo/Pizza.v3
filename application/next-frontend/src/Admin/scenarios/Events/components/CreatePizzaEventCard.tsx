cead060d9522dbf3b1b44849fd01778b9c8e32f8
Stian Sulebak <69514522+stianjsu@users.noreply.github.com>
2023-11-10 12:48:40 +0100

    Pb-47b: configurable event time (#82)
    * feat: adds logic and input for editing event time
    
    * style: changed to css grid to align form
    
    * refactor: styles and comments
    
    * fix: inconsistent form submit on enter keypress

--- application/next-frontend/src/Admin/scenarios/Events/components/CreatePizzaEventCard.tsx
+++ application/next-frontend/src/Admin/scenarios/Events/components/CreatePizzaEventCard.tsx
@@ -7,29 +7,49 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import Button from 'Admin/components/Button'
 
+const formatTimeInput = (inp: string) => {
+    // Remove non digit chars
+    const formatted = inp.replace(/[^\d]/g, '')
+
+    return formatted.slice(0, 2)
+}
+
+const formatTimeInpOnBlur = (inp: string, min: number, max: number) => {
+    // Remove non digit chars
+    let formatted = inp.replace(/[^\d]/g, '')
+
+    if (formatted == '') formatted = '00'
+    let timeNumber = parseInt(formatted)
+
+    if (timeNumber < min) timeNumber = min
+    else if (timeNumber > max) timeNumber = max
+
+    if (timeNumber < 10) return `0${timeNumber}`
+    return `${timeNumber}`
+}
+
 const validationSchema = z.object({
     eventDate: z.number(),
     eventMonth: z.number(),
-    eventTime: z.string(),
+    eventHour: z.string(),
+    eventMinute: z.string(),
     participants: z.nullable(z.number().int()),
 })
 
 type FormData = {
     eventDate: number
     eventMonth: number
-    eventTime: string
+    eventHour: string
+    eventMinute: string
     participants?: number
 }
 
-type Props = {
-    selectedDate: Date
-}
-
-const CreatePizzaEventCard = ({ selectedDate }: Props) => {
-    const [date, time] = [selectedDate.getDate(), '18:00']
+const CreatePizzaEventCard = ({ selectedDate }: { selectedDate: Date }) => {
     const months = Array.from({ length: 12 }, (_, i) => {
         return new Date(0, i).toLocaleString('en-UK', { month: 'long' })
     })
+
+    const date = selectedDate.getDate()
     const currentMonth = selectedDate.getMonth()
     const currentYear = selectedDate.getFullYear()
 
@@ -38,7 +58,8 @@ const CreatePizzaEventCard = ({ selectedDate }: Props) => {
         defaultValues: {
             eventDate: selectedDate.getDate(),
             eventMonth: selectedDate.getMonth(),
-            eventTime: '18:00',
+            eventHour: '18',
+            eventMinute: '00',
             participants: 5,
         },
     })
@@ -58,7 +79,13 @@ const CreatePizzaEventCard = ({ selectedDate }: Props) => {
 
     const onSubmit = (event: FormData) => {
         const restaurant = findRestaurant()
-        const newEventDate = new Date(currentYear, event.eventMonth, event.eventDate, 18)
+        const newEventDate = new Date(
+            currentYear,
+            event.eventMonth,
+            event.eventDate,
+            parseInt(event.eventHour),
+            parseInt(event.eventMinute),
+        )
 
         if (restaurant) {
             const event: ApiEventPost = {
@@ -85,22 +112,19 @@ const CreatePizzaEventCard = ({ selectedDate }: Props) => {
 
             <FormProvider {...methods}>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mt-14 flex font-workSans text-2xl font-semibold italic text-green-primary">
-                        <span className="w-[63%] text-left">Date:</span>
-                        <span className="w-[37%] text-left">Time</span>
-                    </div>
+                    <div className="mt-12 grid grid-cols-2 gap-4 font-workSans font-semibold">
+                        <h3 className="w-full text-left text-2xl italic">Date:</h3>
+                        <h3 className="w-full text-left text-2xl italic">Time:</h3>
 
-                    <div className="mt-5 flex font-workSans text-4xl font-semibold [&>*]:border-2 [&>*]:border-dotted [&>*]:border-[#94DBB6] ">
-                        <span className="p-3 text-green-tertiary">{date}.</span>
-                        <Controller
-                            name="eventDate"
-                            control={control}
-                            defaultValue={date}
-                            render={() => <input type="hidden" value={date} />}
-                        />
-
-                        <div className="ml-2 flex w-[17rem] justify-center p-3">
-                            <span className="text-green-tertiary">{months[currentMonth]}</span>
+                        <div className="flex flex-row items-center text-4xl">
+                            <span className=" text-green-tertiary">{date}.</span>
+                            <span className="pl-4 text-green-tertiary">{months[currentMonth]}</span>
+                            <Controller
+                                name="eventDate"
+                                control={control}
+                                defaultValue={date}
+                                render={() => <input type="hidden" value={date} />}
+                            />
                             <Controller
                                 name="eventMonth"
                                 control={control}
@@ -111,23 +135,64 @@ const CreatePizzaEventCard = ({ selectedDate }: Props) => {
                             />
                         </div>
 
-                        <span className="ml-6 p-3 text-green-quaternary">{time}</span>
-                        <Controller
-                            name="eventTime"
-                            control={control}
-                            defaultValue={time}
-                            render={() => <input type="hidden" value={time} />}
-                        />
-                    </div>
-
-                    <div className="my-4 mt-14 flex justify-evenly">
-                        <Button
-                            onClick={() => closeModal()}
-                            text="Cancel"
-                            buttonStyle="secondary"
-                            className="w-[270px]"
-                        />
-                        <Button text="Create Event" buttonStyle="primary" className="w-[270px]" type="submit" />
+                        <div className="flex flex-row items-center gap-1 text-4xl">
+                            <div className="border-2 border-dotted border-[#94DBB6]">
+                                <Controller
+                                    name="eventHour"
+                                    control={control}
+                                    defaultValue={'18'}
+                                    render={({ field }) => (
+                                        <input
+                                            className="w-16 bg-transparent p-2 text-center hover:bg-green-secondary"
+                                            type="text"
+                                            onChange={(e) => {
+                                                e.target.value = formatTimeInput(e.target.value)
+                                                field.onChange(e)
+                                            }}
+                                            onBlur={(e) => {
+                                                e.target.value = formatTimeInpOnBlur(e.target.value, 0, 23)
+                                                field.onChange(e)
+                                            }}
+                                            value={field.value}
+                                        />
+                                    )}
+                                />
+                            </div>
+                            <span className="text-green-quaternary">:</span>
+                            <div className="border-2 border-dotted border-[#94DBB6]">
+                                <Controller
+                                    name="eventMinute"
+                                    control={control}
+                                    defaultValue={'00'}
+                                    render={({ field }) => (
+                                        <input
+                                            className="w-16 bg-transparent p-2 text-center hover:bg-green-secondary"
+                                            type="text"
+                                            onChange={(e) => {
+                                                e.target.value = formatTimeInput(e.target.value)
+                                                field.onChange(e)
+                                            }}
+                                            onBlur={(e) => {
+                                                e.target.value = formatTimeInpOnBlur(e.target.value, 0, 59)
+                                                field.onChange(e)
+                                            }}
+                                            value={field.value}
+                                        />
+                                    )}
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-8">
+                            <Button
+                                onClick={() => closeModal()}
+                                text="Cancel"
+                                buttonStyle="secondary"
+                                className="w-[270px] hover:border-neutral hover:text-neutral"
+                            />
+                        </div>
+                        <div className="mt-8">
+                            <Button text="Create Event" buttonStyle="primary" className="w-[270px]" type="submit" />
+                        </div>
                     </div>
                 </form>
             </FormProvider>
