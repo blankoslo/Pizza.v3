867bbb7f8236541a4191bab3b4932d0760cbb2c0
Anna <annabjorgo0308@gmail.com>
2023-11-22 22:24:13 +0100

    add first step in third section

---
+++ application/next-frontend/src/Landing/scenarios/ThirdSection.tsx
@@ -0,0 +1,21 @@
+const ThirdSection = () => {
+    return (
+        <div className="h-[100vh] bg-green-light font-queensMedium italic">
+            <div className="ml-8">
+                <div className="flex flex-col text-[88px] text-green-primary">
+                    <span>Fun to use,</span>
+                    <span className="-mt-8">Easy to manage</span>
+                </div>
+            </div>
+            <div className="flex ">
+                <span className="stroke-green-primary text-8xl font-bold text-[#9AE59D]">1.</span>
+                <div className="ml-4 flex flex-col">
+                    <span className="text-5xl font-medium text-green-primary">Choose</span>
+                    <span className="text-5xl font-medium text-green-primary">Channel</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { ThirdSection }
