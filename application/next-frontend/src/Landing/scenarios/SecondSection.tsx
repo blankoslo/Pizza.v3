4578740ca4a3c826e830ed6a764bc315cf2fa22c
Jacob Theisen <jacobtheisen2211@gmail.com>
2023-11-22 21:29:09 +0100

    PB-48c: minor style change

--- application/next-frontend/src/Landing/scenarios/SecondSection.tsx
+++ application/next-frontend/src/Landing/scenarios/SecondSection.tsx
@@ -10,11 +10,11 @@ const SecondSection = () => {
         <section className="flex h-[100vh] flex-col gap-1 bg-green-primary md:grid md:grid-flow-col md:grid-rows-2">
             <div className="flex items-center justify-center md:w-8/12">
                 <span className="ml-8 font-queensMedium text-8xl text-white">
-                    <i>Create an inclusive work culture</i>
+                    <i>Build a work cultrue with PizzaBot and create...</i>
                 </span>
             </div>
             <div className="flex items-center justify-center">
-                <div className="grid grid-cols-1 gap-4 text-2xl font-medium text-white md:grid-flow-col md:grid-rows-2">
+                <div className="grid grid-cols-1 gap-4 text-2xl font-medium text-white md:mt-[-4rem] md:grid-flow-col md:grid-rows-2">
                     <ImageContainer img={SafeEnvironments} text="Safe Environments" />
                     <ImageContainer img={EngagedTeams} text="Engaged Teams" />
                     <ImageContainer img={Friendships} text="Friendship" />
