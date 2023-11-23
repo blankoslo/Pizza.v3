562cf891bc41b851912c719e940742903755225e
Jacob Theisen <jacobtheisen2211@gmail.com>
2023-11-23 11:54:00 +0100

    PB-48c: minor changes to fonts

--- application/next-frontend/src/Landing/scenarios/SecondSection.tsx
+++ application/next-frontend/src/Landing/scenarios/SecondSection.tsx
@@ -9,19 +9,19 @@ const SecondSection = () => {
     return (
         <section className="flex h-[100vh] flex-col gap-1 bg-green-primary md:grid md:grid-flow-col md:grid-rows-2">
             <div className="flex items-center justify-center md:w-8/12">
-                <span className="ml-8 font-queensMedium text-8xl text-white">
-                    <i>Build a work cultrue with PizzaBot and create...</i>
+                <span className="ml-[20%] font-queensMedium text-8xl italic text-white">
+                    Build a work culture with PizzaBot and create...
                 </span>
             </div>
             <div className="flex items-center justify-center">
-                <div className="grid grid-cols-1 gap-4 text-2xl font-medium text-white md:mt-[-4rem] md:grid-flow-col md:grid-rows-2">
+                <div className="grid grid-cols-1 gap-4 font-queensRegular text-2xl text-white md:mt-[-20%] md:grid-flow-col md:grid-rows-2">
                     <ImageContainer img={SafeEnvironments} text="Safe Environments" />
                     <ImageContainer img={EngagedTeams} text="Engaged Teams" />
                     <ImageContainer img={Friendships} text="Friendship" />
                     <ImageContainer img={GoodTraditions} text="Good Traditions" />
                 </div>
             </div>
-            <div className="flex items-center justify-center md:row-span-2 md:mr-8 ">
+            <div className="flex items-center justify-center md:row-span-2 md:mr-[20rem] ">
                 <HappyMascot />
             </div>
         </section>
