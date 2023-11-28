db4e70be975d3a86feb56ede8d04161fb12ae351
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2023-11-28 11:20:03 +0100

    PB-48i: Sixth section (#94)
    PB-48i: Implemented sixth section, created duplicate addtoslackbutton, added svgs
    
    Co-authored-by: sergiosja <sergey.jakobsen@gmail.com>

--- application/next-frontend/src/Landing/index.tsx
+++ application/next-frontend/src/Landing/index.tsx
@@ -1,11 +1,13 @@
 import { FirstSection } from './scenarios/FirstSection'
 import { SecondSection } from './scenarios/SecondSection'
+import { SixthSection } from './scenarios/SixthSection'
 
 const Landing = () => {
     return (
         <>
             <FirstSection />
             <SecondSection />
+            <SixthSection />
         </>
     )
 }
