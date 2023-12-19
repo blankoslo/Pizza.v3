f3d9ec332d38211c80d1775e71d7ca4fd6136993
Stian Sulebak <stianjsu@gmail.com>
2023-12-19 16:32:32 +0100

    fix tweaks rest of modals and adds slack channel modal for consistancy

--- application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
+++ application/next-frontend/src/Admin/scenarios/AdminMainPage.tsx
@@ -1,6 +1,6 @@
 import { Events } from 'Admin/scenarios/Events'
 import { Restaurants } from 'Admin/scenarios/Restaurants'
-import { SlackChannel } from 'Admin/scenarios/SlackChannel'
+import { SlackChannel } from '@/Admin/scenarios/SlackChannel'
 import Image from 'next/image'
 import MascotHappy from 'Admin/assets/MascotHappy.svg'
 
@@ -10,13 +10,16 @@ const AdminMainPage = () => {
     return (
         <div className="grid-background flex items-center justify-center p-16 px-10 lg:p-20">
             <div className={`flex w-full max-w-[1500px] flex-col items-center gap-16`}>
-                <h1 className="w-full text-left font-queensMedium text-6xl">Pizza Admin</h1>
                 <div className="grid w-full grid-cols-1 items-center justify-items-center gap-16 lg:grid-cols-2">
+                    <div>
+                        <h1 className="w-full text-left font-queensMedium text-6xl">Pizza Admin</h1>
+                    </div>
+                    <div />
                     <HoverProvider>
                         <Events />
                     </HoverProvider>
                     <Image
-                        className="row-start-4 lg:row-start-[unset]"
+                        className="row-start-6 lg:row-start-[unset]"
                         priority
                         src={MascotHappy}
                         width={450}
