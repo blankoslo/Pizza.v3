5ffba93e69b183f278cac69b0a5519c84ae2fc23
annabjorgo <60391111+annabjorgo@users.noreply.github.com>
2024-01-02 18:05:52 +0100

    Pb 59 mobile friendly landing page (#109)
    * small styling changes in frontend
    
    * fix scaling issues in all sections
    
    * feat tweaks styling fifth section
    
    * feat: makes footer more responsive
    
    * fix: small styling changes
    
    * chore: revert first section
    
    ---------
    
    Co-authored-by: Stian Sulebak <stianjsu@gmail.com>

--- application/next-frontend/src/Landing/scenarios/FourthSection.tsx
+++ application/next-frontend/src/Landing/scenarios/FourthSection.tsx
@@ -20,38 +20,36 @@ const FourthSection = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-row flex-wrap">
-                    <div className="w-full max-w-[700px] pb-24">
+                <div className="flex w-full max-w-[700px] flex-row flex-wrap">
+                    <div className="-mb-6 w-full max-w-[700px]">
                         <Image src={PizzaInvitation} alt="pizza invitation in slack" />
                     </div>
-                    <div className="relative min-h-[150px] min-w-[250px] flex-1">
-                        <div className="absolute bottom-4 right-[9rem] w-[16em] bg-postit-yellow py-4 pl-4 pr-16 font-workSans text-xl italic leading-6 text-dark">
+                    <div className="relative min-h-[300px] min-w-[250px] flex-1">
+                        <div className="absolute right-[6rem] top-20 w-[11em] bg-postit-yellow px-4 py-5 font-workSans text-lg italic leading-6 text-dark lg:right-[9rem] lg:top-40 lg:w-[16em] lg:text-xl">
                             Invites are sendt <b className="not-italic">privatly</b> on slack. So you can always
                             decline!
                         </div>
                         <Image
-                            className="absolute bottom-0 right-0 z-10"
-                            width={250}
+                            className="absolute right-0 top-0 z-10 w-[150px] max-w-[250px] lg:w-full"
                             src={PizzaBotWithFourArms}
                             alt="pizza bot with four arms"
                         />
                     </div>
                 </div>
 
-                <div className="flex flex-row flex-wrap-reverse justify-end">
-                    <div className="relative min-h-[150px] min-w-[250px] flex-1">
-                        <div className="absolute bottom-4 left-[9rem] w-[16em] bg-postit-yellow py-4 pl-16 pr-4 font-workSans text-xl italic leading-6 text-dark">
+                <div className="flex w-full max-w-[700px] flex-row flex-wrap-reverse justify-end">
+                    <div className="relative min-h-[300px] min-w-[250px] flex-1">
+                        <div className="absolute left-[6rem] top-20 w-[11em] bg-postit-yellow px-4 py-5 font-workSans text-lg italic leading-6 text-dark lg:left-[9rem] lg:top-36 lg:w-[16em] lg:pl-16 lg:text-xl">
                             <b className="not-italic">After everyone accepts,</b> events gets posted on the choses
                             channel
                         </div>
                         <Image
-                            className="absolute bottom-0 left-0 z-10"
-                            width={250}
+                            className="absolute left-0 top-0 z-10 w-[150px] max-w-[250px] lg:w-full"
                             src={PizzaBotWithFourArms}
                             alt="pizza bot with four arms"
                         />
                     </div>
-                    <div className="w-full max-w-[700px] pb-24">
+                    <div className="-mb-4 w-full max-w-[700px]">
                         <Image src={PizzaAnnouncement} alt="pizza invitation in slack" />
                     </div>
                 </div>
--- application/next-frontend/src/Landing/scenarios/SecondSection.tsx
+++ application/next-frontend/src/Landing/scenarios/SecondSection.tsx
@@ -9,36 +9,40 @@ import { AddToSlackButtonWhite } from '@/Landing/components/AddToSlackButtons'
 
 const SecondSection = () => {
     return (
-        <section className="flex items-center justify-center bg-green-primary p-16 lg:px-20">
-            <div className="flex w-full max-w-[1500px] flex-col gap-16 italic text-white">
+        <section className="flex min-h-[100vh] items-center justify-center bg-green-primary p-16 lg:px-20">
+            <div className="flex w-full max-w-[1500px] flex-col italic text-white">
                 {/* Title */}
                 <div className="w-full font-queensRegular text-[60px] lg:text-[88px]">
-                    <span className="inline-block max-w-[10em]">
-                        Build a work culture with the PizzaBot and create...
-                    </span>
+                    <span className="inline-block max-w-[10em]">Together with the PizzaBot create...</span>
                 </div>
-                <div className="flex w-full flex-row flex-wrap justify-around gap-16">
-                    {/* Icon grid */}
-                    <div className="grid grid-cols-2 grid-rows-2 gap-16 px-12 text-center font-workSans text-2xl">
-                        <div className="flex items-center justify-center">
-                            <ImageContainer img={SafeEnvironments} text="Safe Environments" />
+                <div className="flex w-full flex-row justify-around">
+                    {/* Grid def */}
+                    <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+                        {/* Icon grid */}
+                        <div className="grid grid-cols-2 grid-rows-2 gap-4 text-center font-workSans text-2xl lg:px-12">
+                            <div className="flex items-center justify-center">
+                                <ImageContainer img={SafeEnvironments} text="Safe Environments" />
+                            </div>
+                            <div className="flex items-center justify-center">
+                                <ImageContainer img={EngagedTeams} text="Engaged Teams" />
+                            </div>
+                            <div className="flex items-center justify-center">
+                                <ImageContainer img={GoodTraditions} text="Good Traditions" />
+                            </div>
+                            <div className="flex items-center justify-center">
+                                <ImageContainer img={Friendships} text="Friendship" />
+                            </div>
                         </div>
-                        <div className="flex items-center justify-center">
-                            <ImageContainer img={EngagedTeams} text="Engaged Teams" />
+
+                        {/* Bot icon */}
+                        <div className="flex flex-1 justify-end">
+                            <Image
+                                src={HappyMascot}
+                                alt="Happy Mascot"
+                                className="w-full max-w-[20rem] lg:max-w-[30rem]"
+                            />
                         </div>
-                        <div className="flex items-center justify-center">
-                            <ImageContainer img={GoodTraditions} text="Good Traditions" />
-                        </div>
-                        <div className="flex items-center justify-center">
-                            <ImageContainer img={Friendships} text="Friendship" />
-                        </div>
-                        <div className="my-16">
-                            <AddToSlackButtonWhite />
-                        </div>
-                    </div>
-                    {/* Bot icon */}
-                    <div className="flex flex-1 items-end justify-end">
-                        <Image src={HappyMascot} alt="Happy Mascot" className="w-[34rem] min-w-[17rem]" />
+                        <AddToSlackButtonWhite />
                     </div>
                 </div>
             </div>
